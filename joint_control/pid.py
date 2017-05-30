f2e4af9121f10a8cfbdd3b14efcd6b7dd9bc9301
RebecaTU <fernandezniederacher@campus.tu-berlin.de>
2017-05-30 18:47:38 +0200

    fix parameters

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -37,9 +37,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 35
+        self.Ki = 0.3
+        self.Kd = 0.1
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
